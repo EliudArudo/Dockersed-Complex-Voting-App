984593aa18a50437776a8fa066007b5cbebb1ec4
EliudArudo <eliudarudo@gmail.com>
2019-03-09 00:02:52 +0300

    Basic wireframe of cluster

---
+++ voter/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{test}}</div>
+</template>
+
+<script>
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({})
+export default class HelloWorld extends Vue {
+  test = "Hello world";
+}
+</script>
+
+<style>
+</style>
---
+++ voter/src/views/Splashscreen.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="splashscreen">{{test}}</div>
+</template>
+
+<script>
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({})
+export default class SplashScreen extends Vue {
+  test = "dara dara";
+}
+</script>
+
+<style lang="scss">
+</style>
